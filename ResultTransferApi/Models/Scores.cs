8b52de7fb64569555bbed4d60f2601454adb1f41
MantasAM <30369006+MantasAM@users.noreply.github.com>
2017-11-12 19:57:15 +0200

    Add files via upload

---
+++ ResultTransferApi/Models/Scores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResultTransferApi.Models
+{
+    public class Scores
+    {
+        public long Id { get; set; }
+        public int RedTeamScore { get; set; }
+        public int BlueTeamScore { get; set; }
+    }
+}
